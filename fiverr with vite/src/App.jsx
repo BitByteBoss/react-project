76d5cc8984129ff395f3f841a4d13706f002d3fb
Dheeraj Dixit <160017238+BitByteBoss@users.noreply.github.com>
2024-10-07 02:26:12 +0530

    added react route for Home page

--- fiverr with vite/src/App.jsx
+++ fiverr with vite/src/App.jsx
@@ -1,42 +1,25 @@
-import React from "react"
-import Navbar from "./components/navbar/Navbar"
-import Footer from "./components/footer/Footer"
-import Home from "./pages/home/Home"
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
-import Featured from "./components/featured/Featured";
-import Services from "./components/services/Services";
-import Aboutus from "./components/aboutUs/Aboutus";
+// import React from "react"
+// import Navbar from "./components/navbar/Navbar"
+// import Footer from "./components/footer/Footer"
 
-function App() {
+// import Featured from "./components/featured/Featured";
+// import Services from "./components/services/Services";
+// import Aboutus from "./components/aboutUs/Aboutus";
 
-  const Layout = () => {
-    return (
-      <div className="app">
-        <Navbar />
-        <Featured />
-        <Services />
-        <Aboutus />
-        <Footer/>
-      </div>
-    )
-  }
+// function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout/>,
-    },
-  ]);
+//   const Layout = () => {
+//     return (
+//       <div className="app">
+//         <Navbar />
+//         <Featured />
+//         <Services />
+//         <Aboutus />
+//         <Footer/>
+//       </div>
+//     )
+//   }
+// }
 
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  )
-}
 
-export default App
+// export default App
---
+++ fiverr with vite/src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Featured from '../featured/Featured'
+import Services from '../services/Services'
+import Aboutus from '../aboutus/Aboutus'
+
+function Home() {
+  return (
+    <div>
+      <Featured />
+      <Services />
+      <Aboutus />
+    </div>
+  )
+}
+
+export default Home
